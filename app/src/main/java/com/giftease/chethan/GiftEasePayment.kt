9e5af40b0d5d0c8001bb8f9e25a1a0a8b313953d
chetan637 <chetanmithra2@gmail.com>
2024-08-21 08:02:41 +0100

    paymentpage completed

--- app/src/main/java/com/giftease/chethan/GiftEasePayment.kt
+++ app/src/main/java/com/giftease/chethan/GiftEasePayment.kt
@@ -28,6 +28,146 @@ import com.giftease.chethan.ui.theme.TextColor
 
 class PaymentActivity : ComponentActivity() {
 
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            val context = LocalContext.current
+            val gradient45 = Brush.linearGradient(
+                colors = listOf(Color.White, Color.White),
+                start = Offset(0f, Float.POSITIVE_INFINITY),
+                end = Offset(Float.POSITIVE_INFINITY, 0f)
+            )
+
+            Surface(
+                modifier = Modifier
+                    .background(gradient45)
+                    .fillMaxSize()
+                    .padding(28.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(gradient45),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Spacer(modifier = Modifier.height(15.dp))
+                    HeadingTextComponent(value = "Payment Details")
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    var bankName by remember { mutableStateOf("") }
+                    var cardNumber by remember { mutableStateOf("") }
+                    var sortCode by remember { mutableStateOf("") }
+                    var cvvNumber by remember { mutableStateOf("") }
+
+                    OutlinedTextField(
+                        value = bankName,
+                        onValueChange = { bankName = it },
+                        label = { Text("Bank Name", style = TextStyle(color = Color.Blue, fontSize = 18.sp)) },
+                        leadingIcon = { Icon(painterResource(id = R.drawable.card_png1), contentDescription = null) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color.Blue,
+                            unfocusedBorderColor = Color.Gray,
+                            backgroundColor = Color.LightGray
+                        )
+                    )
+
+                    OutlinedTextField(
+                        value = cardNumber,
+                        onValueChange = { cardNumber = it },
+                        label = { Text("Card Number", style = TextStyle(color = Color.Red, fontSize = 16.sp)) },
+                        leadingIcon = { Icon(painterResource(id = R.drawable.card_png1), contentDescription = null) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color.Red,
+                            unfocusedBorderColor = Color.Gray,
+                            backgroundColor = Color.Yellow
+                        )
+                    )
+
+                    OutlinedTextField(
+                        value = sortCode,
+                        onValueChange = { sortCode = it },
+                        label = { Text("Sort Code", style = TextStyle(color = Color.Green, fontSize = 14.sp)) },
+                        leadingIcon = { Icon(painterResource(id = R.drawable.card_png1), contentDescription = null) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color.Green,
+                            unfocusedBorderColor = Color.Gray,
+                            backgroundColor = Color.Cyan
+                        )
+                    )
+
+                    OutlinedTextField(
+                        value = cvvNumber,
+                        onValueChange = { cvvNumber = it },
+                        label = { Text("3 digits Cvv Number", style = TextStyle(color = Color.Magenta, fontSize = 20.sp)) },
+                        leadingIcon = { Icon(painterResource(id = R.drawable.card_png1), contentDescription = null) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color.Magenta,
+                            unfocusedBorderColor = Color.Gray,
+                            backgroundColor = Color.Blue
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Button(
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .heightIn(48.dp),
+                        onClick = {
+                            context.startActivity(Intent(context, OrderGiftEase::class.java))
+                        },
+                        contentPadding = PaddingValues(),
+                        colors = ButtonDefaults.buttonColors(Color.Transparent),
+                        shape = RoundedCornerShape(50.dp),
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .heightIn(48.dp)
+                                .background(
+                                    brush = Brush.horizontalGradient(listOf(TextColor, TextColor)),
+                                    shape = RoundedCornerShape(20.dp)
+                                ),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "Proceed",
+                                fontSize = 18.sp,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
 
